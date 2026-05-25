04324f81c70dfb49fb35e58582c02627688ed9b9
davidochieng99-sudo <davidochieng99@gmail.com>
2026-05-26 01:40:25 +0300

    Update whapay-new.js

--- whapay-new.js
+++ whapay-new.js
@@ -35,6 +35,7 @@ console.log("✅ Firebase connected successfully");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 // Force HTTPS redirect (fixes Google indexing)
