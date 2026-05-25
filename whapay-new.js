f5897b9a18b7ca5f07afd8dceea5fe8624ee5da7
davidochieng99-sudo <davidochieng99@gmail.com>
2026-05-26 02:06:00 +0300

    Update whapay-new.js

--- whapay-new.js
+++ whapay-new.js
@@ -2598,22 +2598,22 @@ function generateMembershipCode() {
 }
 
 app.post('/api/incoming-sms', (req, res) => {
-  // Log EVERYTHING Twilio sends
-  console.log('=== INCOMING SMS DEBUG ===');
-  console.log('Headers:', req.headers);
-  console.log('Body:', req.body);
-  console.log('Raw body:', req.rawBody);
+  const customerNumber = req.body.From;
+  const messageText = req.body.Body || '';
   
-  // Try to extract phone number from different possible locations
-  let customerNumber = req.body.From || req.body.from || req.body.To || '';
-  let messageText = req.body.Body || req.body.body || '';
-  
-  console.log(`Phone: ${customerNumber}`);
-  console.log(`Message: ${messageText}`);
-  
-  // Always send a simple reply
+  console.log(`📨 SMS from ${customerNumber}: ${messageText}`);
+
+  let reply = '';
+  if (messageText.toLowerCase().includes('register')) {
+    const tempId = 'REG_' + Date.now() + '_' + Math.random().toString(36).substring(2, 6);
+    const paymentLink = `https://whapay.space/pay.html?amount=100&purpose=registration&tempId=${tempId}&phone=${encodeURIComponent(customerNumber)}`;
+    reply = `Registration fee is KES 100. Click here to pay: ${paymentLink}`;
+  } else {
+    reply = 'Reply "register" to get your WhaPay membership code.';
+  }
+
   const twiml = new MessagingResponse();
-  twiml.message('We received your message. Thank you!');
+  twiml.message(reply);
   res.type('text/xml').send(twiml.toString());
 });
 
